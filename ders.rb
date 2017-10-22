1725d7b72522868f00f1ad388ea2e865df61a06a
Emir Yakar <emrhnykr2@hotmail.com>
2017-10-22 19:48:45 +0300

    Add files via upload

---
+++ ders.rb
@@ -0,0 +1,69 @@
+#2. Hafta derste yaptıklarımız ---1---
+
+print "Yaşınız:"
+
+yas = gets.chomp.to_i
+
+print "Arkadaşınız Yaşı:"
+
+yas2 = gets.chomp.to_i
+
+
+if yas > yas2 
+	yas3 = yas -yas2 
+	a = yas3
+	print "Arkadaşınızdan #{a} yaş büyüksünüz"
+end
+
+if yas2 > yas
+	yas4 = yas2 - yas
+	b = yas4
+	print "Arkdaşınız sizden #{b} yaş büyük"
+end
+	
+if yas=>yas2 :)
+	print "Arkadaşınızla aynı yaştasınız:)"
+
+#2. Hafta derste yaptıklarımız ---2---
+
+print "Yaşınız:"
+
+yas = gets.chomp.to_i
+
+print "Arkadaşınız Yaşı:"
+
+yas2 = gets.chomp.to_i
+
+
+if yas > yas2 
+	yas3 = yas -yas2 
+	a = yas3
+	print "Arkadaşınızdan #{a} yaş büyüksünüz"
+end
+
+if yas2 > yas
+	yas4 = yas2 - yas
+	b = yas4
+	print "Arkdaşınız sizden #{b} yaş büyük"
+end
+	
+if yas=>yas2 :)
+	print "Arkadaşınızla aynı yaştasınız:)"
+	
+
+#2. Hadta derste yaptıklarımız ---3---
+
+
+dizi = [21499 , 24578 , 24268458 , 7 ,2555645257 , 1 ,0 ,3,9,8,1256,3254,88,69]
+
+dizi.each do |a|
+
+	if a.even?
+		puts a
+	end
+end
+
+
+#2. hafta derste yaptıklarımız ---4---
+
+	
